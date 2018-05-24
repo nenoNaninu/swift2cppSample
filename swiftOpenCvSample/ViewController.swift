d8aa297a648520820ce7a2ca42bed1cf70ba7cb5
nenoNaninu <neno@neno-garden.com>
2018-05-24 18:05:42 +0900

    Initial Commit

---
+++ swiftOpenCvSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftOpenCvSample
+//
+//  Created by neno naninu on 2018/05/24.
+//  Copyright © 2018年 neno naninu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
